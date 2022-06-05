60f71db6b409719efc6cb0cebf94d71ee692bf27
soonho <dbstnsgh2@naver.com>
2022-06-05 13:04:34 +0900

    mail test 수정 - Todo 목록 생성후 template에 전달

--- src/test/java/com/example/mailsender/MailSenderApplicationTests.java
+++ src/test/java/com/example/mailsender/MailSenderApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.mailsender;
 
+import com.example.mailsender.entity.Todo;
 import com.example.mailsender.util.MailUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,16 +8,28 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.PropertySources;
 import org.springframework.test.context.TestPropertySource;
 
+import java.util.*;
+
 @SpringBootTest
 @TestPropertySource(locations = "classpath:application.yml")
 class MailSenderApplicationTests {
+
+    private final static List<Todo> todoList = new ArrayList<>();
+
+    static {
+        for (int i = 1; i <= 10; i++) {
+            todoList.add(new Todo(i, "할 일 _" + i, i % 2 == 0, new Date()));
+        }
+    }
 
     @Autowired
     private MailUtil mailUtil;
 
     @Test
     void contextLoads() throws Exception {
-        mailUtil.sendTemplateMail("dbstnsgh2@naver.com", "메일 테스트", "윤순호", null);
+        Map<String, Object> dataMap = new HashMap<>();
+        dataMap.put("todoList", todoList);
+        mailUtil.sendTemplateMail("dbstnsgh2@naver.com", "메일 테스트", "윤순호", dataMap);
     }
 
 }
