54586b74dcfe7baadadb1be66f6c55b8c52fb50f
soonho <dbstnsgh2@naver.com>
2022-06-05 12:50:01 +0900

    add mail send test

--- src/test/java/com/example/mailsender/MailSenderApplicationTests.java
+++ src/test/java/com/example/mailsender/MailSenderApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.mailsender;
 
+import com.example.mailsender.util.MailUtil;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySources;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application.yml")
 class MailSenderApplicationTests {
 
+    @Autowired
+    private MailUtil mailUtil;
+
     @Test
-    void contextLoads() {
+    void contextLoads() throws Exception {
+        mailUtil.sendTemplateMail("dbstnsgh2@naver.com", "메일 테스트", "윤순호", null);
     }
 
 }
