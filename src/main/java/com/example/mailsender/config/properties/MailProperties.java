f20e59afd1a074e9f3a018404d4a47c5aff27f03
soonho <dbstnsgh2@naver.com>
2022-06-05 12:49:36 +0900

    MailProperties @Setter 추가 - binding 오류 수정

--- src/main/java/com/example/mailsender/config/properties/MailProperties.java
+++ src/main/java/com/example/mailsender/config/properties/MailProperties.java
@@ -1,10 +1,12 @@
 package com.example.mailsender.config.properties;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Getter
+@Setter
 @Component
 @ConfigurationProperties(prefix = "spring.mail")
 public class MailProperties {
